4313395daa169c6a41306de76a47fc95da48b3c9
vincent.rodriguez <vincent.rodriguez@MacBook-Air-de-vincentrodriguez.local>
2022-11-07 12:24:07 +0100

    Testing new comparaison

--- projectScript.py
+++ projectScript.py
@@ -67,9 +67,24 @@ def compareKeysWithArrays(bits, privateKeys):
     print("Time sec =", end - st)
 
 
+def testCompareKeys():
+    loopKeys()
+    print("Starting len keys :", len(privateKeys))
+    resultFile = open("result_512.txt", "a")
+    seen = set()
+    uniq = []
+    for x in privateKeys:
+        if x not in seen:
+            uniq.append(x)
+            seen.add(x)
+    resultFile.write("Finale len keys :")
+    resultFile.write(str(len(seen)))
+    print("Finale len keys :", len(seen))
+
+
 # Lance la création des clés de 256 bits et les compare :
-createKeys(1000001, 256, privateKeys_256)
-compareKeysWithArrays(256, privateKeys_256)
+#createKeys(1000001, 256, privateKeys_256)
+#compareKeysWithArrays(256, privateKeys_256)
 # Lance la création des clés de 512 bits et les compare :
-createKeys(1000001, 512, privateKeys_512)
-compareKeysWithArrays(512, privateKeys_512)
+#createKeys(1000001, 512, privateKeys_512)
+#compareKeysWithArrays(512, privateKeys_512)
